001231390eccc582fbb9c99f25e858fa03a3b5dd
Silas <stmiller2@wisc.edu>
2025-11-24 12:07:34 -0600

    Shortened fig. 5

--- scripts/04_analyses/fig5_plots.py
+++ scripts/04_analyses/fig5_plots.py
@@ -60,7 +60,7 @@ err_df  = pd.DataFrame(err_vals,  index=reps[0].index, columns=reps[0].columns)
 variant_scores = spec['eth'].loc[mean_df.columns, ['f_hiq_norm', 'SE_norm']]
 
 #Plot
-fig, ax = plt.subplots(figsize=(6.5, 3.5))
+fig, ax = plt.subplots(figsize=(5, 4))
 for v in mean_df.columns:
     color = spec_cmap(spec_norm(variant_scores.loc[v, 'f_hiq_norm']))
     ax.plot(mean_df.index, mean_df[v], color=color, linewidth=2.5)
@@ -70,7 +70,7 @@ for v in mean_df.columns:
                     color=color, alpha=0.2)
 ax.annotate("0.4% glucose\nadded",
             xy=(136, mean_df.loc[136].mean()*.98),   # point on the curves
-            xytext=(136, ax.get_ylim()[1]*0.75), # position of text
+            xytext=(175, ax.get_ylim()[1]*0.75), # position of text
             arrowprops=dict(facecolor='black', arrowstyle="->"),
             fontsize=10, ha='center')
 ax.set_xlabel("Time (s)", fontsize = 12)
@@ -108,7 +108,7 @@ for i, v in enumerate(mean_df.columns):
 res_df = pd.DataFrame(results)
 
 #Plot correlation
-fig, ax = plt.subplots(figsize=(4,3.5))
+fig, ax = plt.subplots(figsize=(3.5,3.5))
 ax.errorbar(res_df['score'], res_df['auc_mean'], 
             xerr=res_df['score_err'], yerr=res_df['auc_err'], 
             fmt='o', color='black', alpha = 0.8, ecolor='gray', capsize=3)
@@ -123,7 +123,7 @@ adjust_text(labs, arrowprops=dict(arrowstyle='-', color='gray', lw=0.5))
 r, pval = stats.spearmanr(res_df['score'], res_df['auc_mean'], nan_policy='omit')
 props = dict(boxstyle="round,pad=0.5", edgecolor = 'black', linewidth = .75, facecolor=(1,1,1,.2))
 ax.text(0.93, 0.93, f"Spearman R = {r:.2f}\np = {pval:.2e}",
-        transform = ax.transAxes, fontsize = 10,
+        transform = ax.transAxes, fontsize = 8.5,
         verticalalignment = 'top', ha = 'right',
         bbox = props)
 
@@ -141,87 +141,7 @@ fig.savefig(f'{base_dir}/results/fig5/ethidium_scatter.png',
             bbox_inches = 'tight', dpi = 300, transparent = True)
 
 #%%
-""" FIG 5C: IC50 REGRESSIONS FOR REPRESENTATIVE PH TOLERANT AND SENSITIVE VARIANTS """
-
-COLORS = {6: 'tab:green', 7: 'tab:purple'}
-
-#Load data
-data = pd.read_csv(f'{base_dir}/data/external/pH_ic50_data.csv')
-wt_data = data[data['strain'] == 'WT']
-a105e_data = data[data['strain'] == 'A105E']
-
-#Logistic model to fit ic50 curves
-def logistic(x, mn, mx, IC50, hill):
-    return mn + (mx - mn) / (1 + (x / IC50) ** hill)
-
-#Get curvefit data for strain/pH combo
-def fit_curves(data):
-    results = {}
-    for pH in [6, 7]:
-        pH_data = data[data['pH'] == pH]
-        fits, ic50s = [], []
-
-        for rep in pH_data['replicate'].unique():
-            subset = pH_data[pH_data['replicate'] == rep]
-            x, y = subset['norfloxacin_conc_ng/mL'], subset['OD600']
-
-            guess = [min(y), max(y), np.median(x), 3.5]
-            bounds = (0, [np.inf, np.inf, np.inf, 10])
-            popt, _ = curve_fit(logistic, x, y, p0=guess, bounds=bounds, maxfev=1000)
-
-            x_fit = np.logspace(0, np.log10(x.max()), 100)
-            fits.append(logistic(x_fit, *popt))
-            ic50s.append(popt[2])
-
-        results[pH] = {
-            "x_fit": x_fit,
-            "fits": np.vstack(fits),
-            "ic50s": np.array(ic50s),
-            "scatter": pH_data
-        }
-    return results
-wt_results = fit_curves(wt_data)
-a105e_results = fit_curves(a105e_data)
-
-#Plot
-fig, axes = plt.subplots(2, 1, figsize=(6, 3.5), sharex=True)
-for ax, (strain, results) in zip(axes, [('WT', wt_results), ('A105E', a105e_results)]):
-    for pH, res in results.items():
-        x_fit = res["x_fit"]
-        mean, std = res["fits"].mean(axis=0), res["fits"].std(axis=0)
-
-        #Scatter replicate points
-        for rep in res["scatter"]['replicate'].unique():
-            subset = res["scatter"][res["scatter"]['replicate'] == rep]
-            ax.scatter(subset['norfloxacin_conc_ng/mL'], subset['OD600'], color=COLORS[pH], s=15)
-
-        #Curvefit mean with error
-        ax.plot(x_fit, mean, color=COLORS[pH], label=f'pH {pH}')
-        ax.fill_between(x_fit, mean-std, mean+std, color=COLORS[pH], alpha=0.3)
-        ax.axvline(res["ic50s"].mean(), 0, mean.max()/2.3, ls='--', color=COLORS[pH])
-
-    ax.set_xscale('log')
-    ax.set_ylim(0, 1.21)
-    ax.set_xlim(40, 1500)
-    ax.xaxis.set_major_formatter(plt.ScalarFormatter())
-    ax.xaxis.set_major_locator(plt.LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1))
-    ax.set_xticks([50, 100, 200, 300, 500, 700, 1000])
-
-    if strain == 'WT':
-        ax.legend()
-        ax.set_title('Wild-type NorA')
-    else:
-        ax.set_xlabel('[norfloxacin] (ng/mL)', fontsize = 12)
-        ax.set_title('A105E', y = .96)
-        
-fig.text(0.01, 0.5, 'OD600 at 16 hours', fontsize = 12, va='center', rotation='vertical')
-
-plt.show()
-fig.savefig(f'{base_dir}/results/fig5/representative_ic50s.png', 
-            dpi=300, bbox_inches='tight', transparent = True)
-
-#%%
-""" FIG 5D: CORRELATION OF HIGH-THROUGHPUT AND CLONAL PH SENSITIVITIES """
+""" FIG 5C: CORRELATION OF HIGH-THROUGHPUT AND CLONAL PH SENSITIVITIES """
 
 clones = ['WT', 'T336E', 'H89L', 'F47D', 'I298D', 'N332Q', 'S133E', 'F159K', 'H354Q', 'I76M', 'P121R', 'F188C', 'A105E', 'M150E']
 
@@ -241,7 +161,7 @@ effc = effc.loc[clones]
 r, p = stats.spearmanr(effc['nor_dFpH'], clnl['dFpH'])
 
 #Plot
-fig, ax = plt.subplots(figsize = (4,3.5))
+fig, ax = plt.subplots(figsize = (3.5,3.5))
 ax.errorbar(effc['nor_dFpH'], clnl['dFpH'], 
             xerr=effc['nor_SE'], yerr=clnl['err_dFpH'], 
             fmt='o', color='black', alpha=0.8, ecolor='gray', capsize=3)
@@ -254,7 +174,7 @@ for clone_name in clones:
 
 props = dict(boxstyle="round,pad=0.5", edgecolor = 'black', linewidth = .75, facecolor=(1,1,1,.2))
 ax.text(0.055, 0.95, f"Spearman R = {r:.2f}\np = {p:.3f}",
-        transform = ax.transAxes, fontsize = 10,
+        transform = ax.transAxes, fontsize = 8.5,
         verticalalignment = 'top', ha = 'left',
         bbox = props)
 adjust_text(labs, arrowprops=dict(arrowstyle='-', color='gray', lw=0.5))  
@@ -264,7 +184,7 @@ fig.savefig(f'{base_dir}/results/fig5/validation_scatter.png',
             dpi = 300, bbox_inches = 'tight', transparent = True)
 
 #%%
-""" FIG 5E: HIBIT MEMBRANE-LOCALIZED ABUNDANCE BAR PLOT """
+""" FIG 5D: HIBIT MEMBRANE-LOCALIZED ABUNDANCE BAR PLOT """
 
 #Load data
 df = pd.read_csv(f'{base_dir}/data/external/hibit_data.csv')
@@ -343,8 +263,8 @@ plt.yscale('log')
 plt.ylim(np.log(1.028), np.log(1000))
 plt.xticks(rotation=45, ha='right', rotation_mode = 'anchor', fontsize = 8)
 plt.yticks(fontsize = 10)
-plt.ylabel("Normalized luminescence\n(log)", fontsize = 12, labelpad = -10)
-plt.title("Relative protein abundance")
+plt.ylabel("Normalized luminescence\n(log)", fontsize = 10, labelpad = -10)
+plt.title("Relative protein abundance", fontsize = 11)
 plt.tight_layout()
 
 plt.show()
@@ -358,7 +278,7 @@ hibit_sum['err'] = errs
 hibit_sum.to_csv(f'{base_dir}/results/general/hibit_sum.csv')
 
 #%%
-""" FIG 5F: CORRELATION HEATMAP OF SPECIFICITY, EFFICIENCY, ABUNDANCE """
+""" FIG 5E: CORRELATION HEATMAP OF SPECIFICITY, EFFICIENCY, ABUNDANCE """
 
 # Get specificity data
 spec = sp.load_specificity_data(data_path = f'{base_dir}/data/processed/specificity/specificity_clean.pk1',
@@ -385,7 +305,7 @@ effc = ef.load_efficiency_data(data_path = f'{base_dir}/data/processed/efficienc
 effc.index = effc.index.str[1:]
 
 #Get Rosetta & ThermoMPNN predicted ddGs
-computational_data = pd.read_csv(f'{base_dir}/results/general/computational_stability.csv',
+computational_data = pd.read_csv(f'{base_dir}/data/external/computational_stability.csv',
                                  index_col = 0)
 
 #HiBiT expression data
